422388f374bcc39e7ddef5c0c6e8d3da74df4b78
Vaskman <z.vaskman@gmail.com>
2023-02-04 21:18:34 +0300

    HW-01.02 method- ok ( 3 attempt)

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 public class Main {
     public static void yearCalculation(int yearValues) {
-        if (yearValues % 4 == 0 && yearValues != 100 || yearValues == 400)
+        if (yearValues % 4 == 0 && yearValues % 100 != 0 || yearValues % 400 == 0)
             System.out.println(+yearValues + " год является високосным");
         else
             System.out.println(+yearValues + " год не является високосным");
     }
+
     public static void defineSmartphone(int versionOS, int yearOfIssue) {
         if (versionOS == 0 && yearOfIssue >= 2023) {
             System.out.println("Установите приложение для iOS по ссылке");
@@ -18,48 +19,45 @@ public class Main {
             System.out.println("К сожалению для вашей системы нет приложения");
         }
     }
+
     public static int determineTheDeliveryTime(int a) {
-        int distanceValue=0;
-        if (a > 0 && a <= 20) {
-            distanceValue = distanceValue + 1;}
-        else if (a>20 && a<=60) {
-            distanceValue = distanceValue + 2;
-        } else if (a>60 && a<=100) {
-            distanceValue=distanceValue+3;
-        } else if (a > 100) {
-            distanceValue=distanceValue+4;
+        int distanceValue = 0;
+        if (a > 100) {
+            distanceValue = distanceValue - 1;
         }
         return distanceValue;
     }
+
     public static void consoleСompartment() {
         System.out.println("--------------------");
     }
+
     public static void main(String[] args) {
         consoleСompartment();
         System.out.println("Задача №1");
-        int yearValues = 2023;
+        int yearValues = 800;
         yearCalculation(yearValues);
         consoleСompartment();
 
         System.out.println("Задача №2");
         int versionOS = 1;
         int yearOfIssue = 2022;
-        defineSmartphone(versionOS,yearOfIssue);
+        defineSmartphone(versionOS, yearOfIssue);
         consoleСompartment();
 
         System.out.println("Задача №3");
-        int distance = 103;
-        int l= determineTheDeliveryTime(distance);
-        if (l == 1) {
-            System.out.println("Потребуется дней "+l+" сутки");
-        } else if (l==2) {
-            System.out.println("Потребуется дней "+l+" суток");
-        }else if (l==3) {
-            System.out.println("Потребуется дней "+l+" суток");
-        } else if (l==4) {
+        int distance = 100;
+        int exception = determineTheDeliveryTime(distance);
+        if (exception == -1) {
             System.out.println("Доставки нет");
+        } else if (distance > 0 && distance <= 20) {
+            System.out.println("Потребуется один сутки");
+        } else if (distance > 20 && distance <= 60) {
+            System.out.println("Потребуется двое суток");
+        } else if (distance > 60 && distance <= 100) {
+            System.out.println("Потребуется трое суток");
+            consoleСompartment();
         }
-        consoleСompartment();
     }
 }
 
